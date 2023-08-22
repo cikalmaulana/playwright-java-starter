ba4febe9f9039a9cb760c5bc79908e602887cae3
Rajasa Cikal <79238217+cikalmaulana@users.noreply.github.com>
2023-08-22 13:56:13 +0700

    Update FirstScenarioStepDef.java

--- src/test/java/steps/FirstScenarioStepDef.java
+++ src/test/java/steps/FirstScenarioStepDef.java
@@ -14,24 +14,20 @@ public class FirstScenarioStepDef {
         dashboard = new Dashboard();
     }
 
-    @Given("^access google and search:$")
+    @Given("access google and search")
     public void accessGoogleAndSearch(DataTable param) {
-        List<Map<String,String>> data = param.asMaps(String.class, String.class);
-        for(Map<String,String> searchData : data){
-            String keyword = searchData.get("keywords");
-            String isImage = searchData.get("isImage");
-
-            String keywordsParam = System.getProperty("KEYWORDS_PARAM");
-            System.out.println("keywordsParam = " + keywordsParam);
-            if (keywordsParam != null && !keywordsParam.isEmpty()) {
-                String[] keywords = keywordsParam.split(",");
-                for (String key : keywords) {
-                    dashboard.navigateTo("https://www.google.com/");
-                    dashboard.searchInput(key, isImage);
-                }
-            } else {
-                // Handle the case when KEYWORDS_PARAM is not provided
+        String keywordsParam = System.getProperty("KEYWORDS_PARAM");
+        String isImageParam = System.getProperty("IS_IMAGE_PARAM");
+        System.out.println("keywordsParam = " + keywordsParam);
+        if (keywordsParam != null && !keywordsParam.isEmpty()) {
+            String[] keywords = keywordsParam.split(",");
+            for (String key : keywords) {
+                dashboard.navigateTo("https://www.google.com/");
+                dashboard.searchInput(key, isImageParam);
             }
+        } else {
+            // Handle the case when KEYWORDS_PARAM is not provided
+            System.out.println("Keyword & Image is null!");
         }
     }
 }
