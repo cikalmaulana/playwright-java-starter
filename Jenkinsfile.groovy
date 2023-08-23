046c8766f5193c5487fe63c57fa3c3e689e8d7c0
Rajasa Cikal <79238217+cikalmaulana@users.noreply.github.com>
2023-08-23 14:15:06 +0700

    Create Jenkinsfile.groovy

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,62 @@
+pipeline {
+    agent any
+
+    parameters {
+        string(name: 'KEYWORDS_PARAM', description: 'Comma-separated list of keywords')
+        string(name: 'IS_IMAGE_PARAM', description: 'Whether to use images')
+        choice(name: 'WHICH_FEATURE', choices: ['firstTest', 'secondTest', 'thirdTest'], description: 'Which Feature to Run')
+    }
+
+    stages {
+        stage('Checkout') {
+            steps {
+                // Mengambil kode dari repositori GitHub
+                checkout([$class: 'GitSCM',
+                          branches: [[name: '*/main']],
+                          userRemoteConfigs: [[url: 'https://github.com/cikalmaulana/playwright-java-starter.git']]])
+            }
+        }
+
+        stage('Install Dependencies') {
+            steps {
+                // Menginstal dependensi proyek menggunakan Maven
+                bat 'mvn clean install'
+            }
+        }
+
+        stage('Run Tests') {
+            steps {
+                // Menampilkan PATH sebelum menjalankan perintah mvn
+                bat 'echo $PATH'
+
+                script {
+                    switch (params.WHICH_FEATURE) {
+                        case 'firstTest':
+                            bat 'mvn clean test -Dtest=TestRunner1'
+                            break
+                        case 'secondTest':
+                            bat 'mvn clean test -Dtest=TestRunner2'
+                            break
+                        case 'thirdTest':
+                            bat 'mvn clean test -Dtest=TestRunner3'
+                            break
+                        default:
+                            echo "Invalid choice"
+                            break
+                    }
+                }
+            }
+        }
+
+        stage('Generate Reports') {
+            steps {
+                script {
+                    step([$class: 'CucumberReportsPublisher',
+                          jsonReportDirectory: '**/*.json',
+                          ignoredFailedTests: true,
+                          outputDirectory: 'target/cucumber-reports'])
+                }
+            }
+        }
+    }
+}
